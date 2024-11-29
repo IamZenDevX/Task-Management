a3663e67c3b58648a898aaa7b5483efb9a847fd1
IamZenDevX <zendevx.social@gmail.com>
2024-11-29 15:43:46 +0530

    Update app.js

--- Task Management Project/app.js
+++ Task Management Project/app.js
@@ -1,12 +1,7 @@
 const express = require("express");
-
-// const userRoute = require("./router/userRoute");
 const cors = require("cors");
-// const UserRoute = require("./models/UserModel");
-
 const mongoose = require("mongoose");
 const jwt = require('jsonwebtoken');
-
 const indexfile = require('./router/index')
 
 const app = express();
@@ -17,20 +12,12 @@ app.use(cors());
 app.use('/',indexfile)
 
 
-// app.use("/user" , userRoute);
-
-
-
-
-
-
-
 // Here we are listening to the server
 app.listen(PORT, () => {
   console.log("I am live again");
 });
 
-const uri =`mongodb+srv://admin01:admin01@cluster0.rei7tip.mongodb.net/Task_management?retryWrites=true&w=majority&appName=Cluster0`;
+const uri =`mongodb+srv://name:password@cluster0.rei7tip.mongodb.net/Task_management?retryWrites=true&w=majority&appName=Cluster0`;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
   console.log("Succesfull")
 
